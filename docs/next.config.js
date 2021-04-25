eb1419d231571f64100aa09b790c3a66bc899c4f
Chara- <escor05_11@hotmail.com>
2021-04-25 00:12:53 -0500

    docs - first steps

---
+++ docs/next.config.js
@@ -0,0 +1,4 @@
+// next.config.js
+const withNextra = require('nextra')('nextra-theme-docs', './theme.config.js')
+
+module.exports = withNextra()
---
+++ docs/theme.config.js
@@ -0,0 +1,54 @@
+export default {
+  repository: 'https://github.com/jcarlos0511/bsale-store',
+  docsRepository: 'https://github.com/jcarlos0511/bsale-store5',
+  branch: 'main',
+  path: '/docs',
+  titleSuffix: ' – Test',
+  nextLinks: true,
+  prevLinks: true,
+  search: true,
+  darkMode: true,
+  footer: true,
+  footerText: `${new Date().getFullYear()} © Chara-`,
+  footerEditOnGitHubLink: true,
+  logo: (
+    <>
+      <span className="mr-2 font-extrabold hidden md:inline">BSale store</span>
+      <span className="text-gray-600 font-normal hidden md:inline">
+        Documentación
+      </span>
+    </>
+  ),
+  head: () => (
+    <>
+      <meta name="msapplication-TileColor" content="#ffffff" />
+      <meta name="theme-color" content="#ffffff" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta httpEquiv="Content-Language" content="en" />
+      <meta
+        name="description"
+        content="React 2025 – Build applications from the future, today."
+      />
+      <meta
+        name="og:description"
+        content="React 2025 – Build applications from the future, today."
+      />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:site" content="@leeerob" />
+      <meta
+        name="og:title"
+        content="React 2025 – Build applications from the future, today."
+      />
+      <meta
+        name="twitter:image"
+        content="https://react-2025-docs.vercel.app/og.png"
+      />
+      <meta
+        name="og:image"
+        content="https://react-2025-docs.vercel.app/og.png"
+      />
+      <meta name="apple-mobile-web-app-title" content="React 2025" />
+      <link href="/favicon.ico" rel="shortcut icon" />
+    </>
+  )
+}
